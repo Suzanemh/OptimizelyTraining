b62f6d7a39d0a265b14d9cd7209b46b467a1c13b
Suzane Hamze <suzane.hamze@epinova.no>
2024-09-26 09:17:18 +0200

    Added controller, view etc

---
+++ Controller/SamplePageController.cs
@@ -0,0 +1,12 @@
+﻿using EPiServer.Web.Mvc;
+using Microsoft.AspNetCore.Mvc;
+using SUZTraining.Models.Pages; // Dette er hvor SamplePage-modellen ligger
+
+	public class SamplePageController : PageController<SamplePage>
+	{
+		public ActionResult Index(SamplePage currentPage)
+		{
+			return View(currentPage);
+		}
+	}
+
--- Models/Pages/SamplePage.cs
+++ Models/Pages/SamplePage.cs
@@ -1,9 +1,12 @@
-﻿using EPiServer.Core;
-using EPiServer.DataAnnotations;
-
-[ContentType(DisplayName = "Sample Page", GUID = "b91d4b09-678b-45b6-9f1e-69fb5a8dd79f", Description = "This is a sample page.")]
-public class SamplePage : PageData
+﻿namespace SUZTraining.Models.Pages
 {
-    public virtual string Heading { get; set; }
-    public virtual XhtmlString MainBody { get; set; }
+    using EPiServer.Core;
+    using EPiServer.DataAnnotations;
+
+    [ContentType(DisplayName = "Sample Page", GUID = "b91d4b09-678b-45b6-9f1e-69fb5a8dd79f", Description = "This is a sample page.")]
+    public class SamplePage : PageData
+    {
+        public virtual string Heading { get; set; }
+        public virtual XhtmlString MainBody { get; set; }
+    }
 }
