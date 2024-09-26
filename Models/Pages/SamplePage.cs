e5912bd6043a63cc9a3351a22be9bcc1bd2bdd20
Suzane Hamze <suzane.hamze@epinova.no>
2024-09-26 08:38:59 +0200

    Add project files.

---
+++ Models/Pages/SamplePage.cs
@@ -0,0 +1,9 @@
+﻿using EPiServer.Core;
+using EPiServer.DataAnnotations;
+
+[ContentType(DisplayName = "Sample Page", GUID = "b91d4b09-678b-45b6-9f1e-69fb5a8dd79f", Description = "This is a sample page.")]
+public class SamplePage : PageData
+{
+    public virtual string Heading { get; set; }
+    public virtual XhtmlString MainBody { get; set; }
+}
